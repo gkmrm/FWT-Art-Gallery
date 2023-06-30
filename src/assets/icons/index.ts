c6becb14e649b995b15cf6aa8945549cbe3b70ad
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-06-30 12:49:07 +0400

    add assets icon + reexport

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,53 @@
+import burgerIcon from './buger_icon.svg';
+import changePicIcon from './change-pic_icon.svg';
+import darkIcon from './dark_icon.svg';
+import deleteIcon from './delete_icon.svg';
+import deleteIconLarge from './delete_icon_large.svg';
+import editIcon from './edit_icon.svg';
+import errorIcon from './error_icon.svg';
+import eyeHideIcon from './eye-hide_icon.svg';
+import eyeIcon from './eye_icon.svg';
+import facebookIcon from './facebook_icon.svg';
+import filterIcon from './filter_icon.svg';
+import gearIcon from './gear_icon.svg';
+import instagramIcon from './instagram_icon.svg';
+import lightIcon from './light_icon.svg';
+import minusIcon from './minus_icon.svg';
+import minusIconLarge from './minus_icon_large.svg';
+import plusIcon from './plus_icon.svg';
+import plusIconLarge from './plus_icon_large.svg';
+import rightArrowIcon from './right_arrow_icon.svg';
+import rightArrowIconLarge from './right_arrow_icon_large.svg';
+import searchIcon from './search_icon.svg';
+import succsessIcon from './success_icon.svg';
+import trashIcon from './trash_icon.svg';
+import trashIconLarge from './trash_icon_large.svg';
+import vkIcon from './vk_icon.svg';
+
+export {
+  burgerIcon,
+  changePicIcon,
+  darkIcon,
+  deleteIcon,
+  deleteIconLarge,
+  editIcon,
+  errorIcon,
+  eyeIcon,
+  eyeHideIcon,
+  facebookIcon,
+  filterIcon,
+  gearIcon,
+  instagramIcon,
+  lightIcon,
+  minusIconLarge,
+  minusIcon,
+  plusIcon,
+  plusIconLarge,
+  rightArrowIcon,
+  rightArrowIconLarge,
+  searchIcon,
+  succsessIcon,
+  trashIcon,
+  trashIconLarge,
+  vkIcon,
+};
