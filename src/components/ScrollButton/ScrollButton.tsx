fc481106f2f2c2b0e7e9f3bcdc44cc0a8a8001a9
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-03 12:52:09 +0400

    create ScrollButton component

---
+++ src/components/ScrollButton/ScrollButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import cn from 'classnames/bind';
+
+import { ReactComponent as Arrow } from '@assets/icons/right_arrow_icon.svg';
+
+import styles from './ScrollButton.module.scss';
+
+const cx = cn.bind(styles);
+
+type TScrollButtonProps = {
+  isDarkTheme?: boolean;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const ScrollButton: React.FC<TScrollButtonProps> = ({
+  isDarkTheme = false,
+  children,
+  ...props
+}) => {
+  // Тут функция для скролла будет написана
+  const onScroll = () => {};
+
+  const classNames = cx('Scroll', {
+    [`Scroll_dark`]: isDarkTheme,
+  });
+
+  return (
+    <button onClick={onScroll} type='button' className={classNames} {...props}>
+      <Arrow />
+    </button>
+  );
+};
+
+export default ScrollButton;
