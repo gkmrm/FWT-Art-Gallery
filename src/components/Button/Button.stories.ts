31a534478a7813d5d12e6dbb88b48f0b4f2b539f
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-03 12:50:40 +0400

    create final version of Button

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -3,9 +3,29 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Button from './Button';
 
 const meta: Meta<typeof Button> = {
-  title: 'Project/Button',
+  title: 'Project/ui/buttons/Button',
   component: Button,
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+          The button coponent, has 3 types of display, which change as a result of passing the classname from the parent component.
+
+          1) Default 
+          Created for displaying normal buttons
+
+          2) Text
+          Created to display button as underlined text without background color
+
+          3) Icon
+          Created to display function buttons as a square image. eg: a trash icon button, a gear icon button. 
+
+          Note: 
+          Buttons to display the switching and scrolling of the theme USE components ToogleTheme and Scroll`,
+      },
+    },
+  },
 };
 
 type Story = StoryObj<typeof Button>;
@@ -13,11 +33,9 @@ type Story = StoryObj<typeof Button>;
 export const Default: Story = {
   args: {
     className: 'default',
-    props: {
-      onClick: () => {
-        // eslint-disable-next-line no-console
-        console.log('CLicked');
-      },
+    onClick: () => {
+      // eslint-disable-next-line no-console
+      console.log('CLicked');
     },
     isDarkTheme: false,
     isDisabled: false,
@@ -28,43 +46,26 @@ export const Default: Story = {
 export const Text: Story = {
   args: {
     className: 'text',
-    props: {
-      onClick: () => {
-        // eslint-disable-next-line no-console
-        console.log('CLicked');
-      },
+    onClick: () => {
+      // eslint-disable-next-line no-console
+      console.log('CLicked');
     },
     isDarkTheme: false,
     isDisabled: false,
-    value: 'button text',
+    value: 'Default button',
   },
 };
 
-export const Up: Story = {
+export const Icon: Story = {
   args: {
-    className: 'up',
-    props: {
-      onClick: () => {
-        // eslint-disable-next-line no-console
-        console.log('CLicked');
-      },
+    className: 'icon',
+    onClick: () => {
+      // eslint-disable-next-line no-console
+      console.log('CLicked');
     },
     isDarkTheme: false,
     isDisabled: false,
-  },
-};
-
-export const Functional: Story = {
-  args: {
-    className: 'functional',
-    props: {
-      onClick: () => {
-        // eslint-disable-next-line no-console
-        console.log('CLicked');
-      },
-    },
-    isDarkTheme: false,
-    isDisabled: false,
+    value: '',
   },
 };
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,44 +2,38 @@ import React from 'react';
 
 import cn from 'classnames/bind';
 
-import BaseButton, { TBaseButtonProps } from './BaseButton';
 import styles from './Button.module.scss';
-// import { ReactComponent as FilterIcon } from './filter_icon.svg';
-// import { ReactComponent as PlusIcon } from './plus_icon.svg';
-// import { ReactComponent as TrashIcon } from './trash_icon.svg';
-// import { ReactComponent as Toggle } from './light_icon.svg';
-// import { Icon, IconType } from '../Icon';
 
 const cx = cn.bind(styles);
 
 type TButtonProps = {
-  props: TBaseButtonProps;
   value?: string;
   icon?: React.ReactNode;
   isDarkTheme?: boolean;
   isDisabled?: boolean;
-  className?: 'default' | 'text' | 'up' | 'functional';
-};
+  className?: 'default' | 'text' | 'icon';
+  onClick: () => void;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<TButtonProps> = ({
   className = 'default',
   icon,
-  props,
   value,
   isDarkTheme = false,
-  isDisabled,
+  isDisabled = false,
+  onClick,
+  ...props
 }) => {
-  const classNamesButton = cx('Button', `Button_${className}`, {
+  const classNames = cx('Button', `Button_${className}`, {
     [`Button_${className}_dark`]: isDarkTheme,
     [`Button_${className}_disabled`]: isDisabled,
   });
 
   return (
-    <div>
-      <BaseButton className={classNamesButton} {...props} value={value}>
-        {icon}
-      </BaseButton>
-    </div>
+    <button onClick={onClick} type='button' className={classNames} {...props}>
+      {icon}
+      {className !== 'icon' ? value : ''}
+    </button>
   );
 };
 
