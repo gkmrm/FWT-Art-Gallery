6e24790a708ea005beccb5b49b1e1e45c7282c2a
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-04 10:13:53 +0400

    create Container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import cn from 'classnames/bind';
+
+import styles from './Container.module.scss';
+
+const cx = cn.bind(styles);
+
+export type TContainerProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Container: React.FC<TContainerProps> = ({ className, children }) => (
+  <div className={cx('Container', className)}>{children}</div>
+);
+
+export default Container;
