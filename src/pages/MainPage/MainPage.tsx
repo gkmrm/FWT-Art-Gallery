5c79d63b3ccd84796243b727397d880bfe0d1a18
Gleb <gkmrm98@gmail.com>
2023-07-09 12:01:46 +0400

    add working Container for MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import cn from 'classnames/bind';
 
-// import { Container } from '@components/Container';
+import { Container } from '@components/Container';
 import { Footer } from '@components/Footer';
 import { Navigation2 } from '@components/Navigation2';
 import { useThemeContext } from '@hooks/ThemeConext';
@@ -24,12 +24,13 @@ const MainPage: React.FC<TMainPageProps> = ({ className }) => {
   return (
     <div className={cx(className, 'mainPage', `mainPage_${theme}`)}>
       <Navigation2 theme={theme} />
-      <Grid className={cx('mainPage__grid')}>
-        {testData.map((item) => (
-          <Card {...item} theme={theme} />
-        ))}
-      </Grid>
-      {/* <Container className={cx('mainPage__wrapperPaint')}></Container> */}
+      <Container className={cx('mainPage__wrapperPaint')}>
+        <Grid className={cx('mainPage__grid')}>
+          {testData.map((item) => (
+            <Card {...item} theme={theme} />
+          ))}
+        </Grid>
+      </Container>
       <Footer theme={theme} />
     </div>
   );
