fe12a639b7d1061731d184da6a8609cc5f34b7d4
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-11 17:37:54 +0400

    update MainPage with store

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,10 +5,11 @@ import cn from 'classnames/bind';
 import { Container } from '@components/Container';
 import { Footer } from '@components/Footer';
 import { Header } from '@components/Header';
+import { Loader } from '@components/ui-components/Loader';
 import { useThemeContext } from '@hooks/ThemeConext';
+import { artistsStaticApi } from '@store/services/ArtistsStaticService';
 import { Card } from '@ui-components/Card';
 import { Grid } from '@ui-components/Grid';
-import testData from '@ui-components/Grid/testDataforCardGrid';
 
 import styles from './MainPage.module.scss';
 
@@ -23,16 +24,22 @@ export type TMainPageProps = {
 
 const MainPage: React.FC<TMainPageProps> = ({ className }) => {
   const { theme } = useThemeContext();
+  const { data: artistStatic = [], isLoading } =
+    artistsStaticApi.useFetchArtistsStaticQuery('');
 
   return (
     <div className={cx(className, 'mainPage', `mainPage_${theme}`)}>
       <Header theme={theme} />
       <Container className={cx('mainPage__wrapperPaint')}>
-        <Grid className={cx('mainPage__grid')}>
-          {testData.map((item) => (
-            <Card {...item} id={item.id} theme={theme} />
-          ))}
-        </Grid>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Grid className={cx('mainPage__grid')}>
+            {artistStatic.map((item) => (
+              <Card {...item} id={item.id} theme={theme} onClick={() => {}} />
+            ))}
+          </Grid>
+        )}
       </Container>
       <Footer theme={theme} />
     </div>
