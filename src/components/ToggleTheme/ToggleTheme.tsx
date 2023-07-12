31bc437b9ddb5692c98a3d9393a6ca3ef9b521cf
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-12 15:58:44 +0400

    update toogleTheme

--- src/components/ToggleTheme/ToggleTheme.tsx
+++ src/components/ToggleTheme/ToggleTheme.tsx
@@ -12,19 +12,15 @@ import styles from './ToggleTheme.module.scss';
 const cx = cn.bind(styles);
 
 type TToggleThemeProps = {
-  /**
-   * Additional classNames for ToggleTheme
-   */
   className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const ToggleTheme: React.FC<TToggleThemeProps> = ({ className, ...props }) => {
-  const themeContext = useThemeContext();
+  const { theme, setTheme } = useThemeContext();
 
-  const onThemeChange = () =>
-    themeContext.setTheme(themeContext.theme === 'light' ? 'dark' : 'light');
+  const onThemeChange = () => setTheme(theme === 'light' ? 'dark' : 'light');
 
-  const classNames = cx('toggle', `toggle_${themeContext.theme}`, className);
+  const classNames = cx('toggle', `toggle_${theme}`, className);
 
   return (
     <Button
@@ -33,13 +29,11 @@ const ToggleTheme: React.FC<TToggleThemeProps> = ({ className, ...props }) => {
       className={classNames}
       {...props}
     >
-      <span
-        className={cx('toggle__icon', `toggle__icon_${themeContext.theme}`)}
-      >
-        {themeContext.theme === 'dark' ? <LightIcon /> : <DarkIcon />}
+      <span className={cx('toggle__icon', `toggle__icon_${theme}`)}>
+        {theme === 'dark' ? <LightIcon /> : <DarkIcon />}
       </span>
       <span className={cx(`toggle__text`)}>
-        {themeContext.theme === 'dark' ? 'Dark mode' : 'Light mode'}
+        {theme === 'dark' ? 'Dark mode' : 'Light mode'}
       </span>
     </Button>
   );
