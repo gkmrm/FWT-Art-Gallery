0ddc06d3355d3e402addfe4f835d97623fa9911c
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-14 19:08:44 +0400

    create RTK query api + delete slice

--- src/store/services/ArtistsStaticService.ts
+++ src/store/services/ArtistsStaticService.ts
@@ -1,21 +1,35 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 
-import IArtistsStatic, {
-  IArtistStaticModel,
-  normalizeIArtistStatic,
-} from '@models/ArtistStaticModels';
+import {
+  IArtistsStatic,
+  IArtistsStaticModel,
+  normalizeIArtistsStatic,
+} from '@store/models/ArtistsStaticModels';
+import {
+  IArtistsStaticById,
+  IArtistsStaticByIdModel,
+  normalizeIArtistStaticById,
+} from '@store/models/ArtistStaticByIdModel';
 import BASE_URL from '@utils/BASE_URL';
 
 export const artistsStaticApi = createApi({
   reducerPath: 'artistsStaticApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (build) => ({
-    fetchArtistsStatic: build.query<IArtistStaticModel[], ''>({
+    fetchArtistsStatic: build.query<IArtistsStaticModel[], ''>({
       query: () => ({
         url: '/artists/static',
       }),
-      transformResponse: (response: IArtistsStatic[]): IArtistStaticModel[] =>
-        response.map(normalizeIArtistStatic),
+      transformResponse: (response: IArtistsStatic[]): IArtistsStaticModel[] =>
+        response.map(normalizeIArtistsStatic),
+    }),
+    fetchArtistStaticById: build.query<IArtistsStaticByIdModel, string>({
+      query: (id) => ({
+        url: `/artists/static/${id}`,
+      }),
+      transformResponse: (
+        response: IArtistsStaticById
+      ): IArtistsStaticByIdModel => normalizeIArtistStaticById(response),
     }),
   }),
 });
