3ffc13267d121cbe9e6de1b2cec99f9016af3c47
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-17 17:52:05 +0400

    update Grid component

--- src/components/ui-components/Grid/Grid.tsx
+++ src/components/ui-components/Grid/Grid.tsx
@@ -6,19 +6,10 @@ import styles from './Grid.module.scss';
 
 const cx = cn.bind(styles);
 
-type TGridProps = {
-  /**
-   * Additional classNames for Grid
-   */
-  className?: string;
-  /**
-   * Inner components for this component
-   */
-  children?: React.ReactNode;
-};
+type TGridProps = { className?: string } & React.HTMLAttributes<HTMLDivElement>;
 
-const Grid: React.FC<TGridProps> = ({ className, children }) => (
-  <div className={cx(className, 'grid')}>{children}</div>
+const Grid: React.FC<TGridProps> = ({ className, ...props }) => (
+  <div className={cx(className, 'grid')} {...props} />
 );
 
 export default Grid;
