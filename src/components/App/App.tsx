abc8f9e766b2f65bd4e67888487df802ea3b2039
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-18 18:07:27 +0400

    fix ThemeProviderComponent -> ThemeProvider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,18 +3,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-import { ThemeProviderComponent } from '@context/ThemeConext';
+import { ThemeProvider } from '@context/ThemeConext';
 import { MainPage } from '@pages/MainPage';
 import { setupStore } from '@store/store';
 
 import { ArtistPage } from '../../pages/ArtistPage';
 
 const App = () => {
-  const [theme, setTheme] = useState<ThemeTypes>('light');
   const store = setupStore();
 
   return (
-    <ThemeProviderComponent value={{ theme, setTheme }}>
+    <ThemeProvider>
       <Provider store={store}>
         <BrowserRouter>
           <Routes>
@@ -29,10 +28,5 @@ const App = () => {
     </ThemeProvider>
   );
 };
-const App = () => (
-  <ThemeProviderComponent>
-    <MainPage />
-  </ThemeProviderComponent>
-);
 
 export default App;
