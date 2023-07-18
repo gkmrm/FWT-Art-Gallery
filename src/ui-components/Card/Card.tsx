d81c0e897c0926432bf2b5c7d67bc66ff916d872
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-18 13:41:30 +0400

    fix Card component

--- src/ui-components/Card/Card.tsx
+++ src/ui-components/Card/Card.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import cn from 'classnames/bind';
 
 import { ReactComponent as ArrowCardIcon } from '@assets/icons/arrowIcon.svg';
+import { IImage } from '@store/models/PaintModel';
 import { Link } from '@ui-components/Link';
-import { TPictureProps, Picture } from '@ui-components/Picture';
+import { Picture } from '@ui-components/Picture';
 import { ThemeType } from 'src/context/ThemeConext';
 
 import styles from './Card.module.scss';
@@ -12,41 +13,26 @@ import styles from './Card.module.scss';
 const cx = cn.bind(styles);
 
 export type TCardProps = {
-  /**
-   * Data id from backend
-   */
   id?: string;
-  /**
-   * Author name- string
-   */
   title: string;
-  /**
-   * Name of Paint - string
-   */
   subtitle: string;
   /**
    * Object with image source {string, string...}
    */
-  image: TPictureProps;
-  /**
-   * Callback function to click event
-   */
-  href: string;
-  /**
-   * Bollean value for change theme
-   */
-  theme?: ThemeType;
+  image: IImage;
+  pathTo: string;
+  theme: ThemeType;
 };
 
 const Card: React.FC<TCardProps> = ({
   title,
   subtitle,
   image,
-  href = '/',
+  pathTo = '/',
   theme = 'light',
 }) => (
-  <Link className={cx('card')} href={href} theme={theme}>
-    <Picture {...image} className={cx('card__img')} />
+  <Link className={cx('card')} theme={theme} to={pathTo}>
+    <Picture {...image} className={cx('card__img')} alt={`paint of ${title}`} />
     <div className={cx('card__wrapper')}>
       <div className={cx('info', `info_${theme}`)}>
         <div className={cx('info__textBlock', `info__textBlock_${theme}`)}>
