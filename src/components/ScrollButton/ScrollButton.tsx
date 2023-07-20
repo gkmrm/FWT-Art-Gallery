92cf9db482f5e2b6f0cbf0f8fd4fada445b9ed0c
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-20 18:13:39 +0400

    create ScrollButton logic

--- src/components/ScrollButton/ScrollButton.tsx
+++ src/components/ScrollButton/ScrollButton.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import cn from 'classnames/bind';
 
 import { ReactComponent as ArrowIcon } from '@assets/icons/right_arrow_icon.svg';
-import { ThemeType } from '@context/ThemeConext';
+import { useThemeContext } from '@context/ThemeConext';
 import { Button } from '@ui-components/Button';
 
 import styles from './ScrollButton.module.scss';
 
 const cx = cn.bind(styles);
 
-type TScrollButtonProps = {
-  theme: ThemeType;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+const ScrollButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const { theme } = useThemeContext();
 
-const ScrollButton: React.FC<TScrollButtonProps> = ({ theme, ...props }) => {
-  // TODO Тут функция для скролла будет написана
-  const onScroll: React.MouseEventHandler = () => null;
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-  const classNames = cx('scroll', [`scroll_${theme}`]);
+  useEffect(() => {
+    const buttonVisibility = () => {
+      if (window.scrollY > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', buttonVisibility);
+
+    return () => window.removeEventListener('scroll', buttonVisibility);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
 
   return (
     <Button
-      onClick={() => onScroll}
+      onClick={scrollToTop}
       type='button'
-      className={classNames}
-      {...props}
+      className={cx('scroll', `scroll_${theme}`)}
     >
       <ArrowIcon />
     </Button>
