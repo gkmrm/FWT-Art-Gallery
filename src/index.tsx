7091270d518b876e5a7fead805610ef06ac9477c
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-20 18:13:03 +0400

    relocation Provider component's

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { App } from '@components/App';
+import { ThemeProvider } from '@context/ThemeConext';
+import { setupStore } from '@store/store';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const store = setupStore();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
