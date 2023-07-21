b9dfad5bad68877d6a611f5b277362684b9728e2
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-21 15:28:41 +0400

    update ArtistPage

--- src/pages/ArtistPage/ArtistPage.tsx
+++ src/pages/ArtistPage/ArtistPage.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import cn from 'classnames/bind';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { ReactComponent as ArrowCardIcon } from '@assets/icons/arrowIcon.svg';
 import { ArtistInfo } from '@components/ArtistInfo';
 import { Container } from '@components/Container';
-import { Footer } from '@components/Footer';
-import { Header } from '@components/Header';
+import { SliderTest } from '@components/SliderTest';
 import { useThemeContext } from '@context/ThemeConext';
 import { artistsStaticApi } from '@store/services/ArtistsStaticService';
+import { Button } from '@ui-components/Button';
 import { Card } from '@ui-components/Card';
 import { Grid } from '@ui-components/Grid';
-import { Link } from '@ui-components/Link';
+// import { Link } from '@ui-components/Link';
 import { Loader } from '@ui-components/Loader';
 import { Skeleton } from '@ui-components/Skeleton';
 
@@ -23,17 +23,31 @@ const cx = cn.bind(styles);
 const ArtistPage: React.FC = () => {
   const { theme } = useThemeContext();
   const { id = '' } = useParams();
+  const navigate = useNavigate();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleCloseSlider = useCallback(() => setIsOpen(false), []);
+  const handleShowSlider = (index: number) => () => {
+    setCurrentIndex(index);
+    setIsOpen(true);
+  };
 
   const { data: artist, isLoading } =
     artistsStaticApi.useFetchArtistStaticByIdQuery(id);
 
   return (
     <div className={cx('artistPage', `artistPage_${theme}`)}>
-      <Header />
       <div className={cx('link')}>
-        <Link theme={theme} to='/' className={cx('link__content')}>
+        <Button
+          variant='text'
+          theme={theme}
+          onClick={() => navigate(-1)}
+          className={cx('link__content')}
+        >
           <ArrowCardIcon className={cx('link__arrow')} />
-        </Link>
+        </Button>
       </div>
       {isLoading ? <Loader theme={theme} /> : ''}
       {artist && (
@@ -64,10 +78,10 @@ const ArtistPage: React.FC = () => {
               </Grid>
             ) : (
               <Grid className={cx('artistPage__grid')}>
-                {artist?.paintings.map((item) => (
+                {artist?.paintings.map((item, index) => (
                   <Card
                     image={item.paint}
-                    pathTo='/'
+                    onClick={handleShowSlider(index)}
                     {...item}
                     id={item.id}
                     theme={theme}
@@ -76,9 +90,17 @@ const ArtistPage: React.FC = () => {
               </Grid>
             )}
           </Container>
+          {isOpen && (
+            <SliderTest
+              currentIndex={currentIndex}
+              paintings={artist.paintings}
+              theme={theme}
+              isOpen={isOpen}
+              onCloseSlider={handleCloseSlider}
+            />
+          )}
         </>
       )}
-      <Footer />
     </div>
   );
 };
