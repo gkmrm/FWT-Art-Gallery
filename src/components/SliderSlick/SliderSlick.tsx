4e50f5413f78a0c7e392edc5d10f735f98aa4d00
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-24 13:08:10 +0400

    update Slider

--- src/components/SliderSlick/SliderSlick.tsx
+++ src/components/SliderSlick/SliderSlick.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import cn from 'classnames/bind';
+import { RemoveScrollBar } from 'react-remove-scroll-bar';
 
 import { ReactComponent as ChangePic } from '@assets/icons/change-pic_icon.svg';
 import { ReactComponent as Close } from '@assets/icons/close_icon.svg';
 import { ReactComponent as Edit } from '@assets/icons/edit_icon.svg';
-// import { ReactComponent as ArrowIcon } from '@assets/icons/right_arrow_icon_large.svg';
+import { ReactComponent as ArrowIcon } from '@assets/icons/right_arrow_icon_large.svg';
 import { ReactComponent as Delete } from '@assets/icons/trash_icon.svg';
 import { ThemeType } from '@context/ThemeConext';
 import { IPaintModel } from '@store/models/PaintModel';
@@ -23,7 +24,7 @@ export type TSlider = {
   paintings: IPaintModel[];
   isOpen: boolean;
   onCloseClick: () => void;
-  // currentIndex: number;
+  currentIndex: number;
 };
 
 const SliderSlick: React.FC<TSlider> = ({
@@ -31,101 +32,125 @@ const SliderSlick: React.FC<TSlider> = ({
   paintings,
   isOpen,
   onCloseClick,
-  // currentIndex,
+  currentIndex,
 }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const arr = [];
 
   return (
     <div className={cx('slider', { slider__visible: isOpen })}>
-      <Splide>
-        {paintings?.map((painting, index) => (
-          <SplideSlide className={cx('slider__slide')} key={painting.id}>
-            <Picture
-              original={painting.paint.original}
-              alt={`${painting.title}`}
-              className={cx('slider__slide_img')}
-            />
-            <div className={cx('slider__pagination')}>{`${index + 1}/${
-              paintings.length
-            }`}</div>
-            <Button className={cx('action__close')} onClick={onCloseClick}>
-              <Close />
-            </Button>
-            <Button
-              // TODO СТИЛИ
-              className={cx('action__cover')}
-              variant='text'
-              theme={theme}
-              onClick={() => {}}
-            >
-              <ChangePic />
-              make the cover
-            </Button>
-            <div className={cx('infoBlock', `infoBlock_${theme}`)}>
-              <div className={cx('infoBlock__container')}>
-                <div
-                  className={cx('infoBlock__text', `infoBlock__text_${theme}`)}
-                >
+      <RemoveScrollBar />
+      <Splide
+        options={{
+          pagination: false,
+          fixedWidth: '100vw',
+          fixedHeight: '100vh',
+          lazyLoad: true,
+          omitEnd: true,
+          type: 'fade',
+          start: currentIndex,
+        }}
+        hasTrack={false}
+      >
+        <SplideTrack>
+          {paintings?.map((painting, index) => (
+            <SplideSlide className={cx('slider__slide')} key={painting.id}>
+              <Picture
+                original={painting.paint.original}
+                alt={`${painting.title}`}
+                className={cx('slider__slide_img')}
+              />
+              <div className={cx('slider__pagination')}>{`${index + 1}/${
+                paintings.length
+              }`}</div>
+              <Button className={cx('action__close')} onClick={onCloseClick}>
+                <Close />
+              </Button>
+              <Button
+                // TODO СТИЛИ
+                className={cx('action__cover')}
+                variant='text'
+                theme={theme}
+                onClick={() => {}}
+              >
+                <ChangePic />
+                make the cover
+              </Button>
+              <div className={cx('infoBlock', `infoBlock_${theme}`)}>
+                <div className={cx('infoBlock__container')}>
                   <div
                     className={cx(
-                      'infoBlock__text_subtitle',
-                      `infoBlock__text_subtitle_${theme}`
+                      'infoBlock__text',
+                      `infoBlock__text_${theme}`
                     )}
                   >
-                    {painting.subtitle}
-                  </div>
-                  <div
-                    className={cx(
-                      'infoBlock__text_title',
-                      `infoBlock__text_title_${theme}`
-                    )}
-                  >
-                    {painting.title}
+                    <div
+                      className={cx(
+                        'infoBlock__text_subtitle',
+                        `infoBlock__text_subtitle_${theme}`
+                      )}
+                    >
+                      {painting.subtitle}
+                    </div>
+                    <div
+                      className={cx(
+                        'infoBlock__text_title',
+                        `infoBlock__text_title_${theme}`
+                      )}
+                    >
+                      {painting.title}
+                    </div>
                   </div>
                 </div>
+                <div
+                  className={cx(
+                    'infoBlock__icons',
+                    `infoBlock__icons_${theme}`
+                  )}
+                >
+                  <Button
+                    theme={theme}
+                    variant='icon'
+                    className={cx('infoBlock__button')}
+                    onClick={() => {}}
+                  >
+                    <Edit />
+                  </Button>
+                  <Button
+                    theme={theme}
+                    variant='icon'
+                    className={cx('infoBlock__button')}
+                    onClick={() => {}}
+                  >
+                    <Delete />
+                  </Button>
+                </div>
               </div>
-              <div
-                className={cx('infoBlock__icons', `infoBlock__icons_${theme}`)}
-              >
-                <Button
-                  theme={theme}
-                  variant='icon'
-                  className={cx('')}
-                  onClick={() => {}}
-                >
-                  <Edit />
-                </Button>
-                <Button
-                  theme={theme}
-                  variant='icon'
-                  className={cx('')}
-                  onClick={() => {}}
-                >
-                  <Delete />
-                </Button>
-              </div>
-            </div>
-            {/* <div className={cx('slider__navigation')}>
-                <button
-                  type='button'
-                  className={cx('slider__prev')}
-                  onClick={() => {}}
-                  // disabled={currentSlide === 0}
-                >
-                  <ArrowIcon />
-                </button>
-                <button
-                  type='button'
-                  className={cx('slider__next')}
-                  onClick={() => {}}
-                  // disabled={currentSlide === sliderLength - 1}
-                >
-                  <ArrowIcon />
-                </button>
-              </div> */}
-          </SplideSlide>
-        ))}
+            </SplideSlide>
+          ))}
+        </SplideTrack>
+        <div className='splide__arrows'>
+          <div className={cx('slider__navigation')}>
+            <button
+              type='button'
+              className={cx(
+                'slider__prev',
+                'splide__arrow splide__arrow--prev'
+              )}
+            >
+              <ArrowIcon />
+            </button>
+            <button
+              type='button'
+              className={cx(
+                'splide__arrow splide__arrow--next',
+                'slider__next'
+              )}
+            >
+              <ArrowIcon />
+            </button>
+          </div>
+        </div>
       </Splide>
     </div>
   );
