57323832800195ce8200686b238525c99faa8ca1
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-26 17:49:12 +0400

    create UI Modal

--- src/ui-components/Modal/Modal.tsx
+++ src/ui-components/Modal/Modal.tsx
@@ -1,3 +1,44 @@
-const Modal = () => {};
+import React from 'react';
+
+import cn from 'classnames/bind';
+import ReactDOM from 'react-dom';
+import { RemoveScrollBar } from 'react-remove-scroll-bar';
+
+import { ThemeType } from '@context/ThemeConext';
+
+import styles from './Modal.module.scss';
+
+const cx = cn.bind(styles);
+
+type TModalProps = {
+  isShow: boolean;
+  onHide: () => void;
+  theme: ThemeType;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const Modal: React.FC<TModalProps> = ({ isShow, onHide, theme, ...other }) =>
+  isShow
+    ? ReactDOM.createPortal(
+        <>
+          <RemoveScrollBar />
+          <div
+            className={cx('modal', `modal_${theme}`)}
+            aria-modal
+            aria-hidden
+            tabIndex={-1}
+            role='dialog'
+            onClick={onHide}
+          >
+            <div
+              className={cx('modal__inner')}
+              // onClick={(e) => e.stopPropagation}
+              {...other}
+              // role='presentation'
+            />
+          </div>
+        </>,
+        document.body
+      )
+    : null;
 
 export default Modal;
