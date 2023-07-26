8c3ac5faa0ae4b3554d44a9b8ce4ca235a2f1b60
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-26 17:50:35 +0400

    update App, add new Routes for Modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
 
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
+import { AuthModal } from '@components/AuthModal';
 import { MainLayout } from '@layout/MainLayout';
 import { ArtistPage } from '@pages/ArtistPage';
 import { MainPage } from '@pages/MainPage';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<MainPage />} />
-        <Route path='/artists/static/:id' element={<ArtistPage />} />
-      </Route>
-      <Route path='*' element={<Navigate to='/' replace />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const location = useLocation();
+  const background = location.state && location.state.background;
+
+  return (
+    <>
+      <Routes location={background || location}>
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path='/artists/static/:id' element={<ArtistPage />} />
+          <Route
+            path='/login'
+            element={<AuthModal theme='light' variant='login' />}
+          />
+          {/* <Route path='/signup' element={<Modal isShow={false} />} /> */}
+        </Route>
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+
+      {background && (
+        <Routes>
+          <Route
+            path='login'
+            element={<AuthModal theme='light' variant='login' />}
+          />
+          {/* <Route path='signup' element={<Modal isShow={false} />} /> */}
+        </Routes>
+      )}
+    </>
+  );
+};
 
 export default App;
