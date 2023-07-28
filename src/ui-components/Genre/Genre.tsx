24e85b6f6e9181fea2911b127f3ec8fa74b3821e
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-28 18:10:44 +0400

    update Genre component

--- src/ui-components/Genre/Genre.tsx
+++ src/ui-components/Genre/Genre.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 
 import cn from 'classnames/bind';
 
+import { ReactComponent as CloseIcon } from '@assets/icons/close_icon_small.svg';
 import { ThemeType } from '@context/ThemeConext';
 
 import styles from './Genre.module.scss';
 
 const cx = cn.bind(styles);
 
-type TGenreProps = { theme: ThemeType } & React.HTMLAttributes<HTMLDivElement>;
+type TGenreProps = {
+  theme: ThemeType;
+  className?: string;
+  onClose?: () => void;
+} & React.HTMLAttributes<HTMLDivElement>;
 
-const Genre: React.FC<TGenreProps> = ({ theme, ...props }) => (
-  <div className={cx('genre', `genre_${theme}`)} {...props} />
+const Genre: React.FC<TGenreProps> = ({
+  theme,
+  className = '',
+  onClose,
+  ...props
+}) => (
+  <div className={cx(className, 'genre', `genre_${theme}`)} {...props}>
+    {props.children}
+    {onClose && <CloseIcon className={cx('genre__close')} onClick={onClose} />}
+  </div>
 );
 
 export default Genre;
