81216e29ba53dace60c7a0e3c81ded262c092807
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-07-28 12:27:54 +0400

    create base MultiDropDown

---
+++ src/ui-components/MultiDropDown/MultiDropDown.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import cn from 'classnames/bind';
+
+import styles from './MultiDropDown.module.scss';
+
+const cx = cn.bind(styles);
+
+type TMultiDropDownProps = {};
+
+const MultiDropDown: React.FC<TMultiDropDownProps> = () => (
+  <div>MultiDropDown</div>
+);
+
+export default MultiDropDown;
