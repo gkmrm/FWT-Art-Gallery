c9caf06c3fe0c0208a239b719519c81277361f94
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-04 16:07:32 +0400

    adding button for test FilterBar

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import cn from 'classnames/bind';
 import { uid } from 'uid';
 
 import { Container } from '@components/Container';
+import { FilterBar } from '@components/FilterBar';
 import { useThemeContext } from '@context/ThemeConext';
 import { artistsStaticApi } from '@store/services/ArtistsStaticService';
+import { Button } from '@ui-components/Button';
 import { Card } from '@ui-components/Card';
 import { Grid } from '@ui-components/Grid';
 import { Skeleton } from '@ui-components/Skeleton';
@@ -16,12 +18,22 @@ const cx = cn.bind(styles);
 
 const MainPage: React.FC = () => {
   const { theme } = useThemeContext();
+  const [isShow, setShow] = useState(false);
 
   const { data: artistStatic = [], isLoading } =
     artistsStaticApi.useFetchArtistsStaticQuery('');
 
+  const onOpen = () => setShow(true);
+  const onClose = () => setShow(false);
+
   return (
     <div className={cx('mainPage', `mainPage_${theme}`)}>
+      <div>
+        <Button variant='text' onClick={onOpen} theme={theme}>
+          OPEN MODAL
+        </Button>
+        <FilterBar isShow={isShow} onClose={onClose} theme={theme} />
+      </div>
       <Container className={cx('mainPage__wrapperPaint')}>
         {isLoading ? (
           <Grid>
