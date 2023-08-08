55358338b5892a23d16c3b78b40cddffc79ceae7
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-08 14:02:56 +0400

    update Header - add Search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 import cn from 'classnames/bind';
+import _ from 'lodash';
 
 import { ReactComponent as BurgerIcon } from '@assets/icons/buger_icon.svg';
 import { ReactComponent as Logo } from '@assets/icons/logo.svg';
+import { ReactComponent as LoupeIcon } from '@assets/icons/search_icon.svg';
 import { Container } from '@components/Container';
 import { Menu } from '@components/Menu';
 import { useThemeContext } from '@context/ThemeConext';
+import useOutsideClick from '@hooks/useOutsideClick';
 import { Link } from '@ui-components/Link';
+import { Search } from '@ui-components/Search';
 import { Sidebar } from '@ui-components/Sidebar';
 
 import styles from './Header.module.scss';
@@ -16,23 +20,61 @@ const cx = cn.bind(styles);
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearchOpen, setSearchOpen] = useState(false);
   const { theme } = useThemeContext();
+  const ref = useRef<null | HTMLDivElement>(null);
 
   const toggleOpen = () => setIsOpen(!isOpen);
+
+  const onOpen = () => setSearchOpen(true);
+
+  const onChange = React.useCallback((str: string) => {
+    console.log(str);
+  }, []);
+
+  const handleToggle = useCallback(() => {
+    setSearchOpen(false);
+  }, [setSearchOpen]);
+
+  const debounceSearchQuery = _.debounce(onChange, 650);
+
+  useOutsideClick(ref, handleToggle);
 
   return (
     <header className={cx('header', `header_${theme}`)}>
       <Container className={cx('header__container')}>
         <div className={cx('header__wrapper')}>
           <div className={cx('header__logo')}>
-            <Link theme={theme} to='/'>
+            <Link
+              className={cx({ header__logo_hide: isSearchOpen })}
+              theme={theme}
+              to='/'
+            >
               <Logo className={cx('header__logo_icon')} />
             </Link>
           </div>
           <div className={cx('header__menu')}>
             <Menu theme={theme} />
           </div>
-          <BurgerIcon onClick={toggleOpen} className={cx('header__burger')} />
+          <div className={cx('header__controls')}>
+            <div className={cx('header__searchbar')} ref={ref}>
+              <LoupeIcon
+                onClick={onOpen}
+                className={cx('header__loupe', {
+                  header__loupe_disabled: isSearchOpen,
+                })}
+              />
+              <Search
+                className={cx('header__search', {
+                  header__search_open: isSearchOpen,
+                })}
+                theme={theme}
+                errorMessage=''
+                onChange={debounceSearchQuery}
+              />
+            </div>
+            <BurgerIcon onClick={toggleOpen} className={cx('header__burger')} />
+          </div>
         </div>
       </Container>
       <Sidebar theme={theme} isShow={isOpen} onClose={toggleOpen}>
