e4e56314d272af24172a95798fd936f5bb846eac
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-09 17:55:43 +0400

    create DragCard component

---
+++ src/components/DragCard/DragCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import cn from 'classnames/bind';
+
+import { ReactComponent as DragIcon } from '@assets/icons/drag_icon.svg';
+import { ThemeType } from '@context/ThemeConext';
+import { Button } from '@ui-components/Button';
+
+import styles from './DragCard.module.scss';
+
+const cx = cn.bind(styles);
+
+type TDragCardProps = {
+  id: string;
+  theme: ThemeType;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const DragCard: React.FC<TDragCardProps> = ({
+  id,
+  children,
+  theme,
+  ...other
+}) => {
+  const {
+    // isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition: transition || undefined,
+  };
+
+  return (
+    <div className={cx('drag')} ref={setNodeRef} style={style} {...other}>
+      <Button
+        theme={theme}
+        variant='icon'
+        className={cx('button', `button_${theme}`)}
+        {...attributes}
+        {...listeners}
+      >
+        <DragIcon />
+      </Button>
+      {children}
+    </div>
+  );
+};
+
+export default DragCard;
