9e1d04a65eb63bb1425cd0226cf38038da43ad32
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-11 10:31:35 +0400

    update SLider -> add SliderSlide ccomponent

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
-import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import { Splide, SplideTrack } from '@splidejs/react-splide';
 import cn from 'classnames/bind';
 import { RemoveScrollBar } from 'react-remove-scroll-bar';
 
-import { ReactComponent as ChangePic } from '@assets/icons/change-pic_icon.svg';
-import { ReactComponent as Close } from '@assets/icons/close_icon.svg';
-import { ReactComponent as Edit } from '@assets/icons/edit_icon.svg';
 import { ReactComponent as ArrowIcon } from '@assets/icons/right_arrow_icon_large.svg';
-import { ReactComponent as Delete } from '@assets/icons/trash_icon.svg';
 import { ThemeType } from '@context/ThemeContext';
 import { IPaintModel } from '@store/models/PaintModel';
-import { Button } from '@ui-components/Button';
-import { Picture } from '@ui-components/Picture';
 
 import styles from './Slider.module.scss';
 import '@splidejs/react-splide/css/core';
+import SliderSlide from './SliderSlide';
 
 const cx = cn.bind(styles);
 
@@ -50,71 +45,13 @@ const Slider: React.FC<TSliderProps> = ({
     >
       <SplideTrack>
         {paintings?.map((painting, index) => (
-          <SplideSlide className={cx('slider__slide')} key={painting.id}>
-            <Picture
-              original={painting.paint.original}
-              alt={`${painting.title}`}
-            />
-            <div className={cx('slider__pagination')}>{`${index + 1}/${
-              paintings.length
-            }`}</div>
-            <Button className={cx('slider__close')} onClick={onClose}>
-              <Close />
-            </Button>
-            <Button
-              className={cx('slider__cover')}
-              variant='text'
-              theme={theme}
-              onClick={() => {}}
-            >
-              <ChangePic />
-              make the cover
-            </Button>
-            <div className={cx('infoBlock', `infoBlock_${theme}`)}>
-              <div className={cx('infoBlock__container')}>
-                <div
-                  className={cx('infoBlock__text', `infoBlock__text_${theme}`)}
-                >
-                  <div
-                    className={cx(
-                      'infoBlock__text_subtitle',
-                      `infoBlock__text_subtitle_${theme}`
-                    )}
-                  >
-                    {painting.subtitle}
-                  </div>
-                  <div
-                    className={cx(
-                      'infoBlock__text_title',
-                      `infoBlock__text_title_${theme}`
-                    )}
-                  >
-                    {painting.title}
-                  </div>
-                </div>
-              </div>
-              <div
-                className={cx('infoBlock__icons', `infoBlock__icons_${theme}`)}
-              >
-                <Button
-                  theme={theme}
-                  variant='icon'
-                  className={cx('infoBlock__button')}
-                  onClick={() => {}}
-                >
-                  <Edit />
-                </Button>
-                <Button
-                  theme={theme}
-                  variant='icon'
-                  className={cx('infoBlock__button')}
-                  onClick={() => {}}
-                >
-                  <Delete />
-                </Button>
-              </div>
-            </div>
-          </SplideSlide>
+          <SliderSlide
+            painting={painting}
+            length={paintings.length}
+            index={index}
+            theme={theme}
+            onClose={onClose}
+          />
         ))}
       </SplideTrack>
       <div className='splide__arrows'>
