a69d5150a60bf38cfb88eeb70b36330eccedf9a2
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-11 16:44:34 +0400

    create FilterContext -> first iter

--- src/context/ThemeContext/index.ts
+++ src/context/ThemeContext/index.ts
@@ -1,7 +1,5 @@
 export { default as ThemeProvider } from './ThemeContext';
 
-export { default as ThemeProviderComponent } from './ThemeContext';
-
 export { default as useThemeContext } from './useThemeContext';
 
 export type { ThemeType } from './ThemeContext';
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from '@components/App';
+import { FilterContext } from '@context/FilterContext';
 import { ThemeProvider } from '@context/ThemeContext';
 import { setupStore } from '@store/store';
 
@@ -18,11 +19,13 @@ const store = setupStore();
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
+      <FilterContext>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </FilterContext>
     </ThemeProvider>
   </React.StrictMode>
 );
