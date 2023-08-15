7315335d9019aa1d54090bdb43ef270030b1abc7
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-15 15:45:05 +0400

    update Card

--- src/ui-components/Card/Card.tsx
+++ src/ui-components/Card/Card.tsx
@@ -22,6 +22,7 @@ export type TCardProps = {
   image: IImage;
   pathTo?: string;
   theme: ThemeType;
+  inner?: React.ReactNode;
   onClick?: () => void;
 } & React.HTMLAttributes<HTMLDivElement>;
 
@@ -31,19 +32,19 @@ const Card: React.FC<TCardProps> = ({
   image,
   pathTo = '',
   theme = 'light',
-  children,
+  inner,
   onClick,
 }) => (
   <Link className={cx('card')} theme={theme} onClick={onClick} to={pathTo}>
     <div
       role='presentation'
-      className={cx('card__icon')}
+      className={cx('card__inner')}
       onClick={(e) => {
         e.stopPropagation();
         e.preventDefault();
       }}
     >
-      {children}
+      {inner}
     </div>
     <Picture {...image} className={cx('card__img')} alt={`paint of ${title}`} />
     <div className={cx('card__wrapper')}>
