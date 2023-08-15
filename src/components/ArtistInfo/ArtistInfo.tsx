5bfa771883d02e732afef4b174282635207b060a
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-15 15:59:13 +0400

    update ArtistInfo

--- src/components/ArtistInfo/ArtistInfo.tsx
+++ src/components/ArtistInfo/ArtistInfo.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 import cn from 'classnames/bind';
 
-import { ReactComponent as Line } from '@assets/icons/textBlockLine.svg';
-import { Accordion } from '@components/Accordion';
 import { Container } from '@components/Container';
 import { ThemeType } from '@context/ThemeContext';
 import { IGenreModel } from '@store/models/ArtistStaticByIdModel';
 import { IImage } from '@store/models/PaintModel';
 import { AvatarPlaceholder } from '@ui-components/AvatarPlaceholder';
-import { Genre } from '@ui-components/Genre';
 import { Picture } from '@ui-components/Picture';
 
 import styles from './ArtistInfo.module.scss';
+import ArtistInfoBlock from './ArtistInfoBlock';
 
 const cx = cn.bind(styles);
 
@@ -32,49 +30,44 @@ const ArtistInfo: React.FC<TArtistInfoProps> = ({
   year,
   description,
   genres,
-}) => (
-  <div className={cx('artist', `artist_${theme}`)}>
-    <div className={cx('artist__wrapper')}>
-      <div className={cx('artist__avatar')}>
-        {avatar ? (
-          <Picture
-            {...avatar}
-            alt={`${name} - portrait`}
-            className={cx('artist__avatar_image')}
-          />
-        ) : (
-          <AvatarPlaceholder
-            theme={theme}
-            className={cx('artist__avatar_image')}
-          />
-        )}
+}) => {
+  const avatarImage = avatar ? (
+    <Picture
+      {...avatar}
+      alt={`${name} - portrait`}
+      className={cx('artist__avatar_image')}
+    />
+  ) : (
+    <AvatarPlaceholder theme={theme} className={cx('artist__avatar_image')} />
+  );
 
-        <div
-          className={cx('artist__avatar_text', `artist__avatar_text_${theme}`)}
-        >
-          <div className={cx('artist__year')}>{year}</div>
-          <div className={cx('artist__name', `artist__name_${theme}`)}>
-            {name}
+  return (
+    <div className={cx('artist', `artist_${theme}`)}>
+      <div className={cx('artist__wrapper')}>
+        <div className={cx('artist__avatar')}>
+          {avatarImage}
+          <div
+            className={cx(
+              'artist__avatar_text',
+              `artist__avatar_text_${theme}`
+            )}
+          >
+            <div className={cx('artist__year')}>{year}</div>
+            <div className={cx('artist__name', `artist__name_${theme}`)}>
+              {name}
+            </div>
           </div>
         </div>
+        <Container className={cx('artist__container')}>
+          <ArtistInfoBlock
+            theme={theme}
+            description={description}
+            genres={genres}
+          />
+        </Container>
       </div>
-      <Container className={cx('artist__container')}>
-        <div className={cx('artist__textBlock', `artist__textBlock_${theme}`)}>
-          <Line className={cx('artist__textBlock_line')} />
-          <div className={cx('artist__accordion')}>
-            <Accordion theme={theme} text={description} />
-          </div>
-          <div className={cx('artist__genres')}>
-            {genres.map((item) => (
-              <Genre key={item.id} theme={theme}>
-                {item.name}
-              </Genre>
-            ))}
-          </div>
-        </div>
-      </Container>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArtistInfo;
