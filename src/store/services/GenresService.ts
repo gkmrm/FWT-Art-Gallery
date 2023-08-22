3107d6c327812bbef07f3a30fd8230464b588374
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-22 11:09:33 +0400

    update GenreService

--- src/store/services/GenresService.ts
+++ src/store/services/GenresService.ts
@@ -1,11 +1,12 @@
 import { IGenre } from '@models/GenreModel';
 
+import { URLS_GENRES } from './URLS';
 import { apiGallery } from '../api';
 
 export const genreApi = apiGallery.injectEndpoints({
   endpoints: (build) => ({
     fetchGenres: build.query<IGenre[], null>({
-      query: () => ({ method: 'GET', url: '/genres' }),
+      query: () => ({ method: 'GET', url: URLS_GENRES.genres }),
     }),
   }),
 });
