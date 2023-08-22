215b2aa43d6459317a4f15eb03f7730b6d862569
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-22 11:12:21 +0400

    create GenreModel

--- src/store/models/GenreModel.ts
+++ src/store/models/GenreModel.ts
@@ -1,15 +1,27 @@
+import {
+  IId,
+  IIdModel,
+  normalizeIdRequest,
+  normalizeIdResponse,
+} from './IdModel';
+
 export interface IGenre {
-  _id: string;
+  _id: IId;
   name: string;
 }
 
 export interface IGenreModel {
-  id: string;
+  id: IIdModel;
   name: string;
 }
 
 export const normalizeGenres = (item: IGenre): IGenreModel => ({
   // eslint-disable-next-line no-underscore-dangle
-  id: item._id,
+  id: normalizeIdResponse(item._id),
+  name: item.name,
+});
+
+export const normalizeGenresRequest = (item: IGenreModel): IGenre => ({
+  _id: normalizeIdRequest(item.id),
   name: item.name,
 });
