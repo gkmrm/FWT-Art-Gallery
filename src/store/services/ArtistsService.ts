ef7752ac9e0cbe3642041231e5550a520defe169
Gleb Kartavtsev <gkmrm98@gmmail.com>
2023-08-25 16:52:40 +0400

    update ArtistService

--- src/store/services/ArtistsService.ts
+++ src/store/services/ArtistsService.ts
@@ -6,33 +6,23 @@ import {
 import {
   IArtistsAuth,
   IArtists,
-  IArtistsModel,
   normalizeIArtists,
+  normalizeIdRequest,
+  IArtistsResponse,
 } from '@models/ArtistsModel';
 import { apiGallery } from '@store/api';
 import {
-  IArtistEditModel,
-  normalizeIArtistEditRequest,
-} from '@store/models/ArtistEditModel';
-import { IArtistsParams } from '@store/models/FiltersModel';
-import { IIdModel, normalizeIdRequest } from '@store/models/IdModel';
-import {
-  IPaintEditModel,
-  normalizeIPaintRequsest,
-} from '@store/models/PaintEditModel';
+  IArtistsParamsModel,
+  normalizeParams,
+} from '@store/models/FiltersModel';
 import { IPaint } from '@store/models/PaintModel';
+import { normalizeMainPaintingRequest } from '@store/models/RequestsModels';
 
 import { URLS_ARTIST } from './URLS';
 
-const isArtistsType = (
-  response: IArtists[] | IArtistsAuth
-): response is IArtists[] => {
-  if (Array.isArray(response)) {
-    return true;
-  }
-
-  return false;
-};
+// const isArtistsType = (
+//   response: IArtists[] | IArtistsAuth
+// ): response is IArtists[] => Array.isArray(response);
 
 export const artistApi = apiGallery
   .enhanceEndpoints({
@@ -41,20 +31,30 @@ export const artistApi = apiGallery
   .injectEndpoints({
     endpoints: (build) => ({
       fetchArtists: build.query<
-        IArtistsModel[],
-        { isAuth: boolean; params: IArtistsParams }
+        IArtistsResponse,
+        { isAuth: boolean; params: IArtistsParamsModel }
       >({
         query: ({ isAuth, params }) => ({
           method: 'GET',
           url: isAuth ? URLS_ARTIST.artists : URLS_ARTIST.artistsStatic,
-          params,
+          params: normalizeParams(params),
         }),
         transformResponse: (
           response: IArtists[] | IArtistsAuth
-        ): IArtistsModel[] =>
-          isArtistsType(response)
-            ? response.map(normalizeIArtists)
-            : response.data.map(normalizeIArtists),
+        ): IArtistsResponse =>
+          'data' in response
+            ? {
+                data: response.data.map(normalizeIArtists),
+                meta: response.meta,
+              }
+            : { data: response.map(normalizeIArtists), meta: null },
+
+        // isArtistsType(response)
+        //   ? response.map(normalizeIArtists)
+        //   : {
+        //       data: response.data.map(normalizeIArtists),
+        //       meta: response.meta,
+        //     },
         providesTags: ['Artists'],
       }),
       fetchArtistStaticById: build.query<
@@ -69,63 +69,72 @@ export const artistApi = apiGallery
         }),
         transformResponse: (response: IArtistsById): IArtistsByIdModel =>
           normalizeIArtistById(response),
+        providesTags: ['ArtistById'],
       }),
-      createArtist: build.mutation<IArtists, { data: IArtistEditModel }>({
+      createArtist: build.mutation<IArtists, { data: FormData }>({
         query: ({ data }) => ({
           method: 'POST',
           url: URLS_ARTIST.createArtist,
-          data: normalizeIArtistEditRequest(data),
+          data,
         }),
+        invalidatesTags: ['Artists'],
       }),
-      deleteArtist: build.mutation<string, { id: IIdModel }>({
-        query: ({ id }) => ({
+      deleteArtist: build.mutation<string, { authorId: string }>({
+        query: ({ authorId }) => ({
           method: 'DELETE',
-          url: URLS_ARTIST.deleteArtist(id.id),
-          data: normalizeIdRequest(id),
+          url: URLS_ARTIST.deleteArtist(authorId),
+          data: normalizeIdRequest(authorId),
         }),
+        invalidatesTags: ['Artists'],
       }),
-      editArtist: build.mutation<
-        IArtists,
-        { id: IIdModel; data: IArtistEditModel }
-      >({
+      editArtist: build.mutation<IArtists, { id: string; data: FormData }>({
         query: ({ id, data }) => ({
           method: 'PUT',
-          url: URLS_ARTIST.editArtist(id.id),
-          data: normalizeIArtistEditRequest(data),
+          url: URLS_ARTIST.editArtist(id),
+          data,
         }),
+        invalidatesTags: ['Artists', 'ArtistById'],
       }),
-      updateMainPaint: build.mutation<null, { id: IIdModel }>({
-        query: ({ id }) => ({
+      updateMainPaint: build.mutation<
+        null,
+        { authorId: string; paintId: string }
+      >({
+        query: ({ authorId, paintId }) => ({
           method: 'PATCH',
-          url: URLS_ARTIST.updateMainPainting(id.id),
-          data: normalizeIdRequest(id),
+          url: URLS_ARTIST.updateMainPainting(authorId),
+          data: normalizeMainPaintingRequest(paintId),
         }),
+        invalidatesTags: ['Artists', 'ArtistById'],
       }),
-      addPaint: build.mutation<IPaint, { id: IIdModel; data: IPaintEditModel }>(
-        {
-          query: ({ id, data }) => ({
-            method: 'POST',
-            url: URLS_ARTIST.addPaint(id.id),
-            data: normalizeIPaintRequsest(data),
-          }),
-        }
-      ),
+      addPaint: build.mutation<IPaint, { authorId: string; data: FormData }>({
+        query: ({ authorId, data }) => ({
+          method: 'POST',
+          url: URLS_ARTIST.addPaint(authorId),
+          data,
+        }),
+        invalidatesTags: ['Artists', 'ArtistById'],
+      }),
       editPaint: build.mutation<
         IPaint,
-        { id: IIdModel; paintId: string; data: IPaintEditModel }
+        { authorId: string; paintId: string; data: FormData }
       >({
-        query: ({ id, paintId, data }) => ({
+        query: ({ authorId, paintId, data }) => ({
           method: 'PUT',
-          url: URLS_ARTIST.editPaint(id.id, paintId),
-          data: normalizeIPaintRequsest(data),
+          url: URLS_ARTIST.editPaint(authorId, paintId),
+          data,
         }),
+        invalidatesTags: ['ArtistById'],
       }),
-      deletePaint: build.mutation<string, { id: IIdModel; paintId: string }>({
-        query: ({ id, paintId }) => ({
+      deletePaint: build.mutation<
+        string,
+        { authorId: string; paintId: string }
+      >({
+        query: ({ authorId, paintId }) => ({
           method: 'DELETE',
-          url: URLS_ARTIST.deletePaint(id.id, paintId),
-          data: normalizeIdRequest(id),
+          url: URLS_ARTIST.deletePaint(authorId, paintId),
+          data: normalizeIdRequest(paintId),
         }),
+        invalidatesTags: ['ArtistById'],
       }),
     }),
   });
